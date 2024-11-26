aafb4d3351b959c940fdb84ebfd0b8eb6630b241
Manojvanga56 <Manojv5161@gmail.com>
2024-11-26 18:02:57 -0500

    Add files via upload

---
+++ app/components/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import ChatComponent from "./components/chatComponent";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="bg-slate-800 p-3 w-[800px] rounded-md text-white">
+        <h2>GPT-4 Streaming Chat Application</h2> 
+        <ChatComponent/>
+      </div>
+      
+      </main>
+       
+  );
+}
